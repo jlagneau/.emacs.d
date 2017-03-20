371b898721822fa84c825dc2c4e3b360cadefff2
jlagneau <jlagneau@student.42.fr>
2017-03-20 09:27:00 +0100

    Fix installation

--- install.sh
+++ install.sh
@@ -3,8 +3,11 @@
 if [[ ! -e ~/.cask ]]
 then
     echo "Cloning Cask repo"
-    git clone https://github.com/cask/cask.git ~/.cask
+    git clone https://github.com/cask/cask.git $HOME/.cask
 fi
 
-cd ~/.emacs.d
+pushd $HOME/.emacs.d
+
 $HOME/.cask/bin/cask install
+
+popd
